b717e0bcb8cc5ee74419b4d624baa4eb4867faff
Ajay957 <ajay74.aa82@gmail.com>
2022-12-07 11:45:39 +0530

    first commit

---
+++ src/main/java/com/example/Patient_Backend/controller/PatientController.java
@@ -0,0 +1,9 @@
+package com.example.Patient_Backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+}
